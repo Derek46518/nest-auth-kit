175d3fcea25a25f061ed7f659509f033d72b338c
博士不會寫程式 <intercoursedr@gmail.com>
2025-09-27 01:39:16 +0800

    chore: add TypeScript configuration for testing with Jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
     rootDir: __dirname,
-    preset: 'ts-jest',
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'js', 'json'],
+    transform: {
+        '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+    },
     testMatch: ['<rootDir>/test/**/*.spec.ts', '<rootDir>/test/**/*.e2e-spec.ts'],
     moduleNameMapper: {
-        '@/(.*)': '<rootDir>/src/$1'
+        '@/(.*)': '<rootDir>/src/$1',
+        '^nest-auth-kit$': '<rootDir>/src',
+        '^nest-auth-kit/(.*)$': '<rootDir>/src/$1'
     }
 };
